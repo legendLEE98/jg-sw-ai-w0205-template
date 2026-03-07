e7757020e99fd81a4a0ecbde2a86123af9fcd348
이창원 <gravity980530@gmail.com>
2026-03-08 01:50:41 +0900

    백준 문제 1

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,30 @@
-# 파이썬 문법 - 최댓값 (백준 브론즈3)
-# 문제 링크: https://www.acmicpc.net/problem/2562
+# 문제
+
+# 9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오.
+
+# 예를 들어, 서로 다른 9개의 자연수
+
+# 3, 29, 38, 12, 57, 74, 40, 85, 61
+
+# 이 주어지면, 이들 중 최댓값은 85이고, 이 값은 8번째 수이다.
+
+# 입력
+# 첫째 줄부터 아홉 번째 줄까지 한 줄에 하나의 자연수가 주어진다. 주어지는 자연수는 100 보다 작다.
+
+# 출력
+# 첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.
+
+# 현재 밸류보다 input 밸류가 더 크면
+# 현재 밸류를 inputText 밸류로 바꾼다
+bigValue = 0
+curValue = 0
+
+for i, _ in enumerate(range(0, 9)):
+    inputValue = int(input())
+
+    if bigValue < inputValue:
+        bigValue = inputValue
+        curValue = i + 1
+
+print(bigValue)
+print(curValue)
