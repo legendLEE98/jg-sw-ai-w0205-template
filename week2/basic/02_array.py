26dd54c7d2747e8bdc35f8680b0dd5908e9545cd
이창원 <gravity980530@gmail.com>
2026-03-08 00:12:49 +0900

    최종 제출

--- week2/basic/02_array.py
+++ week2/basic/02_array.py
@@ -53,9 +53,27 @@ matrix = [
     [7,8,9]
 ]
 
-length = len(matrix)
+def rotate_matrix_90(matrix):
+    """
+    2차원 배열을 시계방향으로 90도 회전
+    
+    Args:
+        matrix: N x N 2차원 리스트
+    
+    Returns:
+        회전된 2차원 리스트
+    """
+    # n = len(matrix)
 
-def rotate(matrix):
+    # # TODO: n x n 크기의 새로운 배열을 생성하세요 (0으로 초기화)
+    # arr = [[0]* n for _ in range(n)]
+    # print(arr)
+    # pass
+        
+    # TODO: 원본 배열의 각 요소를 회전된 위치에 배치하세요
+    # 힌트: (i, j) 위치의 요소는 회전 후 (j, n-1-i) 위치로 이동
+
+    # 각 배열을 회전시킨다는건, 각 요소의 위치를 바꾼다는 것.
     length = len(matrix)
     original = copy.deepcopy(matrix)
 
@@ -65,98 +83,43 @@ def rotate(matrix):
         for j in range(length):
             change = length - 1 - i
             matrix[j][change] = original[i][j]
-
+    pass
+    
     return matrix
 
+def print_matrix(matrix):
+    """배열을 보기 좋게 출력하는 헬퍼 함수"""
+    for row in matrix:
+        print(row)
 
-print(rotate(matrix))
-
-# print(matrix)
-            
-# def print_matrix(matrix):
-
-#     for row in matrix:
-#         print(row)
-
-# print_matrix(matrix)
-        
-
-# def print_matrix(matrix):
-#     """배열을 보기 좋게 출력하는 헬퍼 함수"""
-#     for row in matrix:
-#         print(row)
-
-# print_matrix(matrix)
-
-
-
-
-
-# def rotate_matrix_90(matrix):
-#     """
-#     2차원 배열을 시계방향으로 90도 회전
+# 테스트 케이스
+if __name__ == "__main__":
+    # 테스트 케이스 1: 3x3 배열
+    matrix1 = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
     
-#     Args:
-#         matrix: N x N 2차원 리스트
+    print("원본 배열:")
+    print_matrix(matrix1)
+    print("\n회전 후:")
+    rotated1 = rotate_matrix_90(matrix1)
+    print_matrix(rotated1)
+    print()
     
-#     Returns:
-#         회전된 2차원 리스트
-#     """
-#     n = len(matrix)
-
-#     print(n)
+    # 테스트 케이스 2: 4x4 배열
+    matrix2 = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16]
+    ]
     
-#     # TODO: n x n 크기의 새로운 배열을 생성하세요 (0으로 초기화)
-#     for col in range(n):
-#         for row in range(n):
-#             matrix[col][row] = 0
-
-#     pass
-        
-#     # TODO: 원본 배열의 각 요소를 회전된 위치에 배치하세요
-#     # 힌트: (i, j) 위치의 요소는 회전 후 (j, n-1-i) 위치로 이동
-
-#     # 각 배열을 회전시킨다는건, 각 요소의 위치를 바꾼다는 것.
-
-
-#     rotated = n
-#     pass
-    
-#     return rotated
-
-# def print_matrix(matrix):
-#     """배열을 보기 좋게 출력하는 헬퍼 함수"""
-#     for row in matrix:
-#         print(row)
-
-# # 테스트 케이스
-# if __name__ == "__main__":
-#     # 테스트 케이스 1: 3x3 배열
-#     matrix1 = [
-#         [1, 2, 3],
-#         [4, 5, 6],
-#         [7, 8, 9]
-#     ]
-    
-#     print("원본 배열:")
-#     print_matrix(matrix1)
-#     print("\n회전 후:")
-#     rotated1 = rotate_matrix_90(matrix1)
-#     print_matrix(rotated1)
-#     print()
-    
-#     # 테스트 케이스 2: 4x4 배열
-#     matrix2 = [
-#         [1, 2, 3, 4],
-#         [5, 6, 7, 8],
-#         [9, 10, 11, 12],
-#         [13, 14, 15, 16]
-#     ]
-    
-#     print("원본 배열:")
-#     print_matrix(matrix2)
-#     print("\n회전 후:")
-#     rotated2 = rotate_matrix_90(matrix2)
-#     print_matrix(rotated2)
+    print("원본 배열:")
+    print_matrix(matrix2)
+    print("\n회전 후:")
+    rotated2 = rotate_matrix_90(matrix2)
+    print_matrix(rotated2)
 
 
