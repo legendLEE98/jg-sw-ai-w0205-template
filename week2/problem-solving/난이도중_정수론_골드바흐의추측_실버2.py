7d5c9fd6ccb96894ddf78be189190c190a452046
이창원 <gravity980530@gmail.com>
2026-03-10 00:53:45 +0900

    골드바흐

--- week2/problem-solving/난이도중_정수론_골드바흐의추측_실버2.py
+++ week2/problem-solving/난이도중_정수론_골드바흐의추측_실버2.py
@@ -27,16 +27,16 @@ import math
 # 1. 받은 밸류의 모든 소수를 찾는 알고리즘이 필요함 - 완료
 # 문제 다시 읽자
 # 2보다 큰 모든 짝수는 소수의 합계로 나타낼 수 있다.
-# 그러면 1만까지의 소수를 먼저 구해야겠네.
 
 # 2. 밸류를 [i] 순서로 빼서, 남은 밸류가 소수인지 판별하는 알고리즘도 필요함
 # 2-1. 밸류를 [n, m]으로 보관하는 2차원 배열이 필요함
 # 3. 최종적으로 n,m 중 n-m 혹은 m-n이 가장 낮은 수를 찾아야 함.
 
+
 counting = int(input())
 inputValue = []
 primeList = []
-candidate = []
+
 
 # inputValue는 primeList의 특정 위치의 값이 들어갈 예정
 # 즉, primeList[i]가 될 예정
@@ -50,24 +50,27 @@ def findPrime(prime):
             primeList.append(i)
             continue
         
+        # 정상 진행방식 시작. 루트 씌워봄
         result = root(i)
 
         # 이미 if에서 2랑 3을 걸러오기 때문에 상관없음.
         for j in range(2, result + 1):
+            # i % j가 0인 순간 나눠졌다는거니, 이건 소수가 아님.
             if i % j == 0:
                 flag = True
                 
         if flag == False:
+            # flag가 마지막까지 false라면 소수목록에 추가.
             primeList.append(i)
 
+# 이건 루트 씌우는 함수
 def root(value):
     result = math.sqrt(value)
     return int(result)
 
 
-for i in range(0, counting):
-    inputValue.append(int(input()))
-
+# input값 중 가장 큰 숫자를 호출하는 함수.
+# 이거 갈기면 자동으로 소수까지 정해짐.
 def bigValue():
     curVal = 0
     for i in range(0, len(inputValue)):
@@ -75,13 +78,53 @@ def bigValue():
             curVal = inputValue[i]
     findPrime(curVal)
 
-bigValue()
-print(primeList)
-print(inputValue)
 
-# 이제 여기서 값 찾기
+# 처음 밸류를 받으면 반복해서 받을 숫자들.
+for i in range(0, counting):
+    inputValue.append(int(input()))
+
+
+candidate = []
+resultList = []
+
 def result():
+    # 이걸로 input값 확인해서 소수 구해옴
+    bigValue()
+
+    primeset = set(primeList)
     for i in range(0, counting):
+        # 그럼 요 안에 후보리스트 구하는거 만들어야겠네.
+        for j in range(0, len(primeList)):
+            sub = inputValue[i] - primeList[j]
+            
+            # 소수가 맞을 때
+            if sub in primeset:
+                if sub <= primeList[j]:
+                    candidate.append([sub, primeList[j]])
+                    break
+
+result()
+for i in candidate:
+    print(i[0], i[1])
+# primeList중에 해당 값이 포함하는지 체크하는 로직이 있으면 좋을 것 같은데
+
+# counting은 = 받은 숫자 갯수
+# 내가 찾아야 할 것
+# candidate[0] 의 길이만큼 회전하기
+
+
+# 지금 갖고 있는게 
+# candidate가 어떤 문제인지 [0] 번째 후보
+# candidate의 최소값 n / 최대값 m
+
+
+# input 받은 값을 한번 빼봄.
+# 뭐부터? prime 밸류부터.
+# inputValue - prime 값을 했는데.
+# 그 값이 primeValue가 아니라면?
+# 다음거 진행해야지.
+# 뭘해야해? 3을 해야해.
+
         
 
 # 값 찾기 반복 횟수는 카운팅 횟수로 해야함.
