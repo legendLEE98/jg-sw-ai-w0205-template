a218e0b6ae0bce05c3919d59942921fbc7613140
이창원 <gravity980530@gmail.com>
2026-03-16 02:32:40 +0900

    basic 정리

--- week3/basic/09_hash_table.py
+++ week3/basic/09_hash_table.py
@@ -41,11 +41,20 @@ def manage_grades(students):
         평균, 최고점 학생 이름, 최고점
     """
     # TODO: 평균 점수 계산
-    pass
-    
-    
-    # TODO: 최고 점수 학생 찾기
-    pass
+    top_score = 0
+    top_student = ""
+    total = 0
+    count = 0
+
+    for name, score in students.items():
+        total += score
+        count += 1
+        if score >= top_score:
+            top_score = score
+            top_student = name
+
+    average = total / count
+
     
     return average, top_student, top_score
 
@@ -60,8 +69,11 @@ def find_student_score(students, name):
     Returns:
         점수 (없으면 None)
     """
+    if students.get(name):
+        return students[name]
+    else:
+        return None
     # TODO: students에서 name 찾기
-    pass
 
 # 테스트 케이스
 if __name__ == "__main__":
