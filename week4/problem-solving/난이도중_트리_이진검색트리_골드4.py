ad6cb3d4a4a40c9efabe49a7d2c6e7b6fb9255ca
이창원 <gravity980530@gmail.com>
2026-03-22 20:41:40 +0900

    이진 검색 트리

--- week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
+++ week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
@@ -1,2 +1,48 @@
 # 트리 - 이진 검색 트리 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/5639
+import sys
+from collections import deque
+
+sys.setrecursionlimit(20000)
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+input = sys.stdin.read().split()
+inputValue = deque(map(int,input))
+
+def makeTree(inputValue:deque, minimum, maximum):
+    if not inputValue:
+        return None
+    
+    if not (minimum < inputValue[0] < maximum):
+        return None
+    
+    root = TreeNode(inputValue.popleft())
+    # min 
+    # 다음값이 최소한 미니멈보단 크면서, 현재값보단 작은거 
+    root.left = makeTree(inputValue, minimum, root.val)
+    # max
+    # 다음 값이 최소한 현재보단 크면서, 맥시멈보단 작은 값
+    root.right = makeTree(inputValue, root.val, maximum)
+
+    return root
+
+def postorder(root:TreeNode):
+    resultList = []
+    
+    if root is None:
+        return []
+    
+    resultList += postorder(root.left)
+    resultList += postorder(root.right)
+    resultList.append(root.val)
+    
+    return resultList
+tree = makeTree(inputValue, 0, float("inf"))
+result = postorder(tree)
+for i in range(len(result)):
+    print(result[i])
--- week4/problem-solving/난이도중_트리_트리의부모찾기_실버2.py
+++ week4/problem-solving/난이도중_트리_트리의부모찾기_실버2.py
@@ -1,2 +1,10 @@
 # 트리 - 트리의 부모 찾기 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/11725
+
+inputNode = int(input())
+vector = []
+
+for i in range(1, inputNode):
+    vector.append(list(map(int, input().split())))
+
+print(vector)
