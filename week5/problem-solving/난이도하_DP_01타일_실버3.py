5528caaa834473f382955f3bd9c05d044257d12e
이창원 <gravity980530@gmail.com>
2026-03-27 21:10:55 +0900

    dp 타일

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,2 +1,15 @@
 # DP - 01타일 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/1904
+
+
+n = int(input())
+
+dp = [0] * (n + 2)
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, n + 1):
+    dp[i] = (dp[i-1] + dp[i-2]) % 15746
+    pass
+
+print(dp[n])
--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,13 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+n = int(input())
+
+fib = []
+fib.append(0)
+fib.append(1)
+
+for i in range(2, n+1):
+    fib.append(fib[i-1] + fib[i-2])
+
+# print(fib[n])
