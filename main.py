d1707d3b30b838019bae2a99a345de0fc4a47a06
이창원 <gravity980530@gmail.com>
2026-03-28 15:13:21 +0900

    동전 문제

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-print("\    /\\")
-print(" )  ( ')")
-print("(  /  )")
-print(" \(__)|")
+# 실험 케이스
+testCase = int(input())
+
+# 잔돈
+change = []
+# 동전 종류
+coins = [25, 10, 5 ,1]
+result = [[] for _ in range(testCase)]
+
+for _ in range(testCase):
+    change.append(int(input()))
+
+for i in range(testCase):
+    for j in range(len(coins)):
+        result[i].append(change[i] // coins[j])
+        change[i] = change[i] % coins[j]
+
+for i in range(len(result)):
+    print(" ".join(map(str, result[i])))
--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,17 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+
+money = list(map(int, input().split()))
+
+coins = []
+for i in range(money[0]):
+    coins.append(int(input()))
+
+result = 0
+for i in range(len(coins) - 1, -1, -1):
+    result += money[1] // coins[i]
+    money[1] = money[1] % coins[i]
+
+print(result)
+
+# for i in range(len(coins)):
