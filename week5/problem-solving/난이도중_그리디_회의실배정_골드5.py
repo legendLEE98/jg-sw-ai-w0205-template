72ee6aae753281967f415f66120fe2e32556bacd
이창원 <gravity980530@gmail.com>
2026-03-30 11:40:49 +0900

    회의실 배정

--- week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
+++ week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
@@ -1,2 +1,21 @@
 # 그리디 - 회의실 배정 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1931
+
+import sys
+input = sys.stdin.readline
+
+order = int(input())
+
+reserv = []
+for i in range(order):
+    reserv.append(list(map(int, input().split())))
+
+reserv.sort(key=lambda x: x[1])
+
+result = 0
+time = 0
+for start, end in reserv:
+    if start >= time:
+        time = end
+        result += 1
+print(result)
